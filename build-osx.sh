21ed35438e53344176d9199a38ebb50860fc7576
John Axel Eriksson <john@insane.se>
2018-03-08 22:53:48 +0100

    Always shut down the os x box after build.

--- build-osx.sh
+++ build-osx.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+at_exit() {
+    echo "Shutdown"
+    shutdown -h now ## always kill this box regardless of exit status
+}
+sig_at_exit() {
+    trap '' EXIT
+    at_exit
+}
+trap at_exit EXIT
+trap sig_at_exit INT QUIT TERM
+
 echo "build on osx"
 sleep 30
+exit 1
 echo "build complete"
